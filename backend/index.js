9eceffbc0305d2f72515e84891a5808c54dddb71
Olha <189846437+sibaraku@users.noreply.github.com>
2026-02-23 20:02:55 +0200

    Added Sequelize and verified database connection

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const Sequelize = require("sequelize");
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    rocess.env.DB_PASSWORD, 
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mysql'
+    });
+
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("DATABASE CONNECTED");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
