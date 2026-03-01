022d5ccd10f85a0c98e25c8f1cbea043e87d388e
Olha <189846437+sibaraku@users.noreply.github.com>
2026-03-01 19:35:50 +0200

    Installed cors package

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const sequelize = require("./config/db");
 const articleRoutes = require("./routes/article.routes");
 
 const app = express();
+app.use(cors());
+
 app.use(express.json());
 
 sequelize.sync({ alter: true })
