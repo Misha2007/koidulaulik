18b5c8bc2211a91d59238a9877b5452fa94028c4
Misha2007 <55316381+Misha2007@users.noreply.github.com>
2026-03-02 15:36:40 +0200

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Activity from "./Activity";
 import { useParams } from "react-router-dom";
 import { IoMdArrowDropright } from "react-icons/io";
@@ -6,14 +6,47 @@ import { IoMdArrowDropright } from "react-icons/io";
 const { VITE_API_URL } = import.meta.env;
 
 function Login() {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const saveUserDataHandler = (enteredUserData) => {
+    const userData = {
+      ...enteredUserData,
+    };
+    props.onLoginUser(userData);
+  };
+
+  const sumbitHandler = (event) => {
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value
+    event.preventDefault();
+
+    if (enteredEmail.trim().length == 0 || enteredPassword.trim().length == 0) {
+      setError({
+        title: "Invalid input",
+        message:
+          "Please enter a valid title or amount or date (non-empty values)",
+      });
+      return;
+    }
+
+    const expenseData = {
+      email: enteredEmail,
+      password: enteredPassword,
+    };
+
+    saveUserDataHandler(expenseData);
+  };
+  
   return (
     <form className="space-y-4 p-8">
       <div>
         <label className="block text-[var(--color-darkbrown)] font-semibold mb-1">
-          Username
+          Email
         </label>
         <input
-          type="text"
+          ref={emailInputRef}
+          type="email"
           placeholder="Enter your username"
           className="w-full px-4 py-2 rounded-lg border border-[var(--color-lightbrown)] focus:outline-none"
         />
@@ -24,6 +57,7 @@ function Login() {
           Password
         </label>
         <input
+          ref={passwordInputRef}
           type="password"
           placeholder="Enter your password"
           className="w-full px-4 py-2 rounded-lg border border-[var(--color-lightbrown)] focus:outline-none"
